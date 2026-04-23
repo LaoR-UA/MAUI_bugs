17facefb744744ffa1c3e855a5f76d98386f8c05
Vladyslav Danylov <vladyslav.danylov@trinetix.com>
2026-04-23 12:04:46 +0300

    Add project files.

---
+++ TestMauiControls/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TestMauiControls;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TestMauiControls/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TestMauiControls;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
